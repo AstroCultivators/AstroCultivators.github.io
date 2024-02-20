f4af5e7637097f97627396ac483c592256d2b16b
coding-yost <77023930+coding-yost@users.noreply.github.com>
2024-02-19 22:30:23 -0800

    Gathered more images and files, created an about section and gallery.

---
+++ scripts/about.js
@@ -0,0 +1,15 @@
+const galleryFolder = './assets/pictures/gallery/';
+const images = [
+    `${galleryFolder}/field_trip.jpg`,
+    `${galleryFolder}/field_trip1.jpg`,
+    `${galleryFolder}/field_trip2.jpg`,
+    `${galleryFolder}/orin_strapped.jpg`,
+    `${galleryFolder}/arcs_event.jpg`
+];
+var count = 0;
+$('#gallery-image').attr('src', images[images.length]);
+setInterval(() => {
+    $('#gallery-image').attr('src', images[count%images.length]);
+    count++;
+}, 4000);
+    
